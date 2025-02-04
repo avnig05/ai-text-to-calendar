dc91988a3417ff49bcab4fcaf2b35daed0d92b80
anuragchillarige <pinganurag@gmail.com>
2025-02-03 22:10:36 -0800

    front end sending request to back end

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from nlp.text_parser import TextToEventParser
 
@@ -11,6 +12,14 @@ class CalendarRequest(BaseModel):
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all domains for now (change in production)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 @app.get("/")
 async def root():
     return {"message": "hello world"}
@@ -22,4 +31,5 @@ async def add_to_calendar(item: CalendarRequest):
     event = parser.parse_text(item.event_body)
     event.set_gcal_link()
 
+    print(event.title)
     return event
