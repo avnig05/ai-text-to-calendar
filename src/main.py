362d0eb64385b0e45b781b575dbea09d26ba69e5
anuragchillarige <pinganurag@gmail.com>
2025-02-17 20:47:44 -0800

    got the image uploading logic coded out on the backend

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
+from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from event_generation.nlp_parsers.text_parser import TextToEventParser
+from event_generation.nlp_parsers.image_parser import ImageToTextParser
 # from event_generation.event.event import Event
 
 class CalendarRequest(BaseModel):
@@ -9,6 +12,8 @@ class CalendarRequest(BaseModel):
     platform: str
 
 app = FastAPI()
+UPLOAD_FOLDER = Path("uploads")
+UPLOAD_FOLDER.mkdir(exist_ok=True)
 
 app.add_middleware(
     CORSMiddleware,
@@ -32,3 +37,12 @@ async def add_to_calendar(item: CalendarRequest):
 
     print(event)
     return event
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    file_path = UPLOAD_FOLDER / file.filename
+
+    with file_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    return {"file name": file.filename}
