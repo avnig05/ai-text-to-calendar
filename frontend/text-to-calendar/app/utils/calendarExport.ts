9a73b16f24747780cdc011e157dfa4818e24a9d1
KeeperOfTheFire <mwratcliff@gmail.com>
2025-02-20 02:44:54 -0800

    linting

--- frontend/text-to-calendar/app/utils/calendarExport.ts
+++ frontend/text-to-calendar/app/utils/calendarExport.ts
@@ -10,26 +10,26 @@ export const exportToOutlook = (event: CalendarEvent) => {
 	window.open(url);
 };
 
-export const exportToAppleCalendar = (event: CalendarEvent) => {
-	const startTime = event.start_time
-		.toISOString()
-		.replace(/-|:|\.\d\d\d/g, "");
-	const endTime = event.endTime.toISOString().replace(/-|:|\.\d\d\d/g, "");
+// export const exportToAppleCalendar = (event: CalendarEvent) => {
+// // 	const startTime = event.start_time
+// // 		.toISOString()
+// // 		.replace(/-|:|\.\d\d\d/g, "");
+// // 	const endTime = event.endTime.toISOString().replace(/-|:|\.\d\d\d/g, "");
 
-	const url = `data:text/calendar;charset=utf-8,BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-DTSTART:${startTime}
-DTEND:${endTime}
-SUMMARY:${event.title}
-DESCRIPTION:${event.description}
-END:VEVENT
-END:VCALENDAR`;
-
-	const link = document.createElement("a");
-	link.href = encodeURI(url);
-	link.download = "event.ics";
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
-};
+// // 	const url = `data:text/calendar;charset=utf-8,BEGIN:VCALENDAR
+// // VERSION:2.0
+// // BEGIN:VEVENT
+// // DTSTART:${startTime}
+// // DTEND:${endTime}
+// // SUMMARY:${event.title}
+// // DESCRIPTION:${event.description}
+// // END:VEVENT
+// // END:VCALENDAR`;
+// // event = event;
+// // 	const link = document.createElement("a");
+// // 	link.href = encodeURI(url);
+// // 	link.download = "event.ics";
+// // 	document.body.appendChild(link);
+// // 	link.click();
+// // 	document.body.removeChild(link);
+// };
